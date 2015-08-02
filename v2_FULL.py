eba54b637e65102648fc772a2b1e461b8d9add36
Surachate <surachate.frank@gmail.com>
2015-08-03 00:26:06 +0700

    FULL

--- v2_FULL.py
+++ v2_FULL.py
@@ -2,6 +2,26 @@
 import cv2
 import math
 
+
+      
+colors = []
+Y_max = 0
+Degree = 0
+Y_MAX_Degree = []
+
+
+def nothing(x):
+    pass
+
+
+def on_mouse_click (event, x, y,flags,param):
+     global range_colors
+     global colors
+     if event == cv2.EVENT_LBUTTONUP:
+          colors = img[y,x].tolist()
+     
+
+               
 def cal():
      if(temp_x[1]>temp_x[2]):
       try:
@@ -66,16 +86,12 @@ def cal():
           cv2.putText(img,str(Degree_4),(temp_x[4]-70,temp_y[4]),cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255),3)
       except:pass
      
-Y_max = 0
-Degree = 0
-Y_MAX_Degree = []
 
 def check_max(y):
      global Y_max
      global Y_MAX_Degree
      if(y >= Y_max):
           Y_max = y
-          #print "Y_MAX = %d"%Y_max
           Y_MAX_Degree.append(Degree)
           
 def SHOW_Y_MAX_Degree():
@@ -88,58 +104,68 @@ def SHOW_Y_MAX_Degree():
           temp = temp / (i/2)
      return temp
 
-#cap = cv2.VideoCapture('process_6dot.avi')
 cap = cv2.VideoCapture(0)
+st = np.zeros((100,500,3), np.uint8)
+cv2.namedWindow('setting')
+cv2.createTrackbar('Range','setting',30,150,nothing)
+cv2.createTrackbar('Area','setting',30,1000,nothing)
+cv2.createTrackbar('switch', 'setting',0,1,nothing)
+cv2.putText(st,"          by SurachateCPESWU",(7,90),cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255),1)
+cv2.putText(st,"Press q to exit.",(7,40),cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),2)
+
+
 while(cap.isOpened()):
      ret, img = cap.read()
-     #temp_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HLS_FULL)
-     if(img != None):
-          process = cv2.blur(img,(3,3))
-          process = cv2.inRange(process,(90,40,10),(160,90,100),img)
-          contours,hierarchy = cv2.findContours(process,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-          temp_x=[]
-          temp_y=[]
-          for cnt in contours :
-               count_M = 0
-               M = cv2.contourArea(cnt)
-               if( M > 30 ):
-                    count_M = count_M + 1
-                    M = cv2.moments(cnt)
-                    cx = int(M['m10']/M['m00'])
-                    cy = int(M['m01']/M['m00'])
-                    cv2.circle(img,(cx,cy),10,(0,255,0),1)
-                    temp_x.append(cx)
-                    temp_y.append(cy)
-          i = len(temp_x)
-          if(i == 6):
-               if(temp_x[1]>temp_x[2]):
-                    cv2.line(img,(temp_x[0],temp_y[0]),(temp_x[1],temp_y[1]),(0,255,0),3)
-                    cv2.line(img,(temp_x[1],temp_y[1]),(temp_x[3],temp_y[3]),(0,255,0),3)
-                    cv2.line(img,(temp_x[3]-100,temp_y[3]),(temp_x[3],temp_y[3]),(255,0,0),3)
-                    cv2.line(img,(temp_x[3],temp_y[3]),(temp_x[5],temp_y[5]),(0,255,0),3)
-                    cv2.line(img,(temp_x[5],temp_y[5]),(temp_x[4],temp_y[4]),(0,255,0),3)
-                    cv2.line(img,(temp_x[4],temp_y[4]),(temp_x[2],temp_y[2]),(0,255,0),3)
-                    cv2.line(img,(temp_x[1],temp_y[1]),(temp_x[1],temp_y[0]+40),(255,0,0),2)
-                    cal()
-               else:
-                    cv2.line(img,(temp_x[0],temp_y[0]),(temp_x[2],temp_y[2]),(0,255,0),3)
-                    cv2.line(img,(temp_x[2],temp_y[2]),(temp_x[3],temp_y[3]),(0,255,0),3)
-                    cv2.line(img,(temp_x[3],temp_y[3]),(temp_x[5],temp_y[5]),(0,255,0),3)
-                    cv2.line(img,(temp_x[5],temp_y[5]),(temp_x[4],temp_y[4]),(0,255,0),3)
-                    cv2.line(img,(temp_x[4],temp_y[4]),(temp_x[1],temp_y[1]),(0,255,0),3)
-                    cv2.line(img,(temp_x[1],temp_y[1]),(temp_x[1],temp_y[0]+40),(255,0,0),2)
-                    cal()
-          #check_max(temp_y[0])
-          #cv2.imshow("hsv",temp_hsv)
+     if(ret==True):
+          range_colors = cv2.getTrackbarPos('Range','setting')
+          range_area = cv2.getTrackbarPos('Area','setting')
+          switch = cv2.getTrackbarPos('switch','setting')
+          process = img.copy()
+          if(colors!=[]):
+               process = cv2.inRange(process,(colors[0]-range_colors,colors[1]-range_colors,colors[2]-range_colors),(colors[0]+range_colors,colors[1]+range_colors,colors[2]+range_colors),img)
+               contours,hierarchy = cv2.findContours(process,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+               temp_x=[]
+               temp_y=[]
+               for cnt in contours :
+                    count_M = 0
+                    M = cv2.contourArea(cnt)
+                    if( M > range_area ):
+                         count_M = count_M + 1
+                         M = cv2.moments(cnt)
+                         cx = int(M['m10']/M['m00'])
+                         cy = int(M['m01']/M['m00'])
+                         cv2.circle(img,(cx,cy),10,(0,255,0),1)
+                         temp_x.append(cx)
+                         temp_y.append(cy)
+               i = len(temp_x)
+               if(i == 6):
+                    if(temp_x[1]>temp_x[2]):
+                         cv2.line(img,(temp_x[0],temp_y[0]),(temp_x[1],temp_y[1]),(0,255,0),3)
+                         cv2.line(img,(temp_x[1],temp_y[1]),(temp_x[3],temp_y[3]),(0,255,0),3)
+                         cv2.line(img,(temp_x[3]-100,temp_y[3]),(temp_x[3],temp_y[3]),(255,0,0),3)
+                         cv2.line(img,(temp_x[3],temp_y[3]),(temp_x[5],temp_y[5]),(0,255,0),3)
+                         cv2.line(img,(temp_x[5],temp_y[5]),(temp_x[4],temp_y[4]),(0,255,0),3)
+                         cv2.line(img,(temp_x[4],temp_y[4]),(temp_x[2],temp_y[2]),(0,255,0),3)
+                         if(switch==1):
+                              cv2.line(img,(temp_x[1],temp_y[1]),(temp_x[1],temp_y[0]+40),(255,0,0),2)
+                         cal()
+                    else:
+                         cv2.line(img,(temp_x[0],temp_y[0]),(temp_x[2],temp_y[2]),(0,255,0),3)
+                         cv2.line(img,(temp_x[2],temp_y[2]),(temp_x[3],temp_y[3]),(0,255,0),3)
+                         cv2.line(img,(temp_x[3],temp_y[3]),(temp_x[5],temp_y[5]),(0,255,0),3)
+                         cv2.line(img,(temp_x[5],temp_y[5]),(temp_x[4],temp_y[4]),(0,255,0),3)
+                         cv2.line(img,(temp_x[4],temp_y[4]),(temp_x[1],temp_y[1]),(0,255,0),3)
+                         if(switch==1):
+                              cv2.line(img,(temp_x[1],temp_y[1]),(temp_x[1],temp_y[0]+40),(255,0,0),2)
+                         cal()
           cv2.imshow("img",img)
-          cv2.imshow("pro",process)
+          cv2.imshow("setting",st)
+          cv2.setMouseCallback('img',on_mouse_click)
+          
+          
      if cv2.waitKey(33) & 0xFF == ord('q'):
           cv2.destroyAllWindows()
           cap.release()
           break
      
-average = SHOW_Y_MAX_Degree()
-#print "##########################"
-#print "Average @ เวลายืดสุดขา = %d"%average
-#print "##########################"
 print "END"
